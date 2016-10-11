f512d2ee1ee3ff5f6179c0a737682e97f5121e64
“zhangquanhong <zhangquanhong@ucfgroup.com>
2016-10-11 16:40:11 +0800

    增加hframe扩展框架实现，增加redis，kafka工具类实现

--- jqgrid-demo/src/main/java/HelloJava.java
+++
@@ -1,9 +0,0 @@
-/**
- * User: zhangqh6
- * Date: 2015/12/10 12:20:20
- */
-public class HelloJava {
-    public static void main(String[] args) {
-        System.out.println("hello java");
-    }
-}
